c178f8552e043b0085e9a93cd6c10e37af5e5a28
Kurt Christensen <kurtc@bitbakery.com>
2014-02-05 15:27:01 -0600

    Initial commit of tutorial files to GitHub Pages implementation of bitbakery.com

---
+++ using/UsingRuby.rb
@@ -0,0 +1,66 @@
+
+
+# We can define a method, all by itself
+def say_hello(name)
+  "Hello, #{name.capitalize}"
+end
+
+puts say_hello "kurt"
+
+# In Ruby everything is an object, or can be gotten to as an object if necessary
+m = self.method(:say_hello)
+puts m.call "Kurt"
+
+
+# Arrays
+a = [123, "Hello", :Kurt]
+puts a[0]
+puts a[1]
+puts a[2]
+a[1] = nil
+puts a
+
+
+# Shortcut for creating an array of words:
+a = %w{ this is an array of words }
+puts a[3]
+
+# Shorthand for appending to an array:
+a << "which"
+a << "keeps"
+a << "growing"
+puts a
+
+# Hashes
+h = {
+    good: "Ruby",
+    better: "Smalltalk",
+    best: "Lisp"
+}
+puts h
+puts h[:best]
+
+
+# We can define modules, which act as function namespaces, and also work as mixins for classes
+module Helpers
+  def enlarge(str)
+    enlarged = ""
+    str.each_char do |c|
+      enlarged << c << " "
+    end
+    enlarged
+  end
+end
+
+# And of course we can define our own classes
+class Person
+  include Helpers
+  attr_accessor :name
+  attr_accessor :age
+end
+
+p = Person.new
+p.name = "Kurt"
+p.age = 42
+
+puts p.enlarge "Wubba"
