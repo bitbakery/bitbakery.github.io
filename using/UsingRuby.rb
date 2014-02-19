9819609d87c5cfec455c802eee5f92fad88cf1b4
Kurt Christensen <kurtc@bitbakery.com>
2014-02-18 18:20:09 -0600

    Adding content to Using Rails

--- using/UsingRuby.rb
+++ using/UsingRuby.rb
@@ -5,15 +5,15 @@ def say_hello(name)
   "Hello, #{name.capitalize}"
 end
 
-puts say_hello "kurt"
+puts say_hello 'kurt'
 
 # In Ruby everything is an object, or can be gotten to as an object if necessary
 m = self.method(:say_hello)
-puts m.call "Kurt"
+puts m.call 'Kurt'
 
 
 # Arrays
-a = [123, "Hello", :Kurt]
+a = [123, 'Hello', :Kurt]
 puts a[0]
 puts a[1]
 puts a[2]
@@ -26,17 +26,18 @@ a = %w{ this is an array of words }
 puts a[3]
 
 # Shorthand for appending to an array:
-a << "which"
-a << "keeps"
-a << "growing"
+a << 'which'
+a << 'keeps'
+a << 'growing'
 puts a
 
 # Hashes
 h = {
-    good: "Ruby",
-    better: "Smalltalk",
-    best: "Lisp"
+    good: 'Ruby',
+    better: 'Smalltalk',
+    best: 'Lisp'
 }
+
 puts h
 puts h[:best]
 
@@ -44,23 +45,33 @@ puts h[:best]
 # We can define modules, which act as function namespaces, and also work as mixins for classes
 module Helpers
   def enlarge(str)
-    enlarged = ""
+    enlarged = ''
     str.each_char do |c|
-      enlarged << c << " "
+      enlarged << c << ' '
     end
     enlarged
   end
 end
 
 # And of course we can define our own classes
-class Person
-  include Helpers
+class Person include Helpers
   attr_accessor :name
   attr_accessor :age
 end
 
 p = Person.new
-p.name = "Kurt"
+p.name = 'Kurt'
 p.age = 42
 
-puts p.enlarge "Wubba"
+puts p.enlarge 'Wubba'
+
+if p.age > 60
+  puts 'NOW you know something'
+elsif p.age > 40
+  puts 'You;re starting to learn...'
+else
+  puts 'You know very little'
+end
+
+# For readability, we can also append conditionals to the end of a statement
+puts 'Kurt is middle-aged' if p.age > 40
